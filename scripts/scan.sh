20a069649dbdc0627b522689699214c14cc1c9ea
Xieonie <alex.sucheuwan@gmail.com>
2025-09-25 09:37:37 +0200

    K: add credit card scanner
    L: add social security scanner
    
    M: add phone number scanner

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,3 @@
-# TODO: write the script
+printf "\n===== SCANNING FOR PHONE NUMBERS =====\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======== PHONE NUMBER SCAN COMPLETE ========="
