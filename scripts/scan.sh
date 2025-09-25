11644bf3c2066769d92744097e370a6775bc086b
Xieonie <alex.sucheuwan@gmail.com>
2025-09-25 10:20:19 +0200

    P: remove scan.sh
    This reverts commit 20a069649dbdc0627b522689699214c14cc1c9ea.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,3 +1,1 @@
-printf "\n===== SCANNING FOR PHONE NUMBERS =====\n"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======== PHONE NUMBER SCAN COMPLETE ========="
+# TODO: write the script
